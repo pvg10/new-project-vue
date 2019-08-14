21adaf060cecd5ba1b274263fdd44c2cbec5ee5b
parth <parthgharpure94@gmail.com>
2019-08-14 17:20:01 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <Home
+      v-bind:state="state"
+      v-bind:handleGreet="handleGreet"
+    />
+  </div>
+</template>
+
+<script>
+import Home from "./Home";
+
+export default {
+  name: "app",
+  components: {
+    Home
+  },
+  methods: {
+    handleGreet: function(event) {
+      this.state.seen = !this.state.seen;
+    }
+  },
+  data() {
+    return {
+      state: {
+        msg: "Hello World",
+        greet: "Good Evening",
+        seen: true,
+        todos: [
+          { text: "Learn JavaScript" },
+          { text: "Learn Vue" },
+          { text: "Build something awesome" }
+        ]
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: round;
+  padding: 0;
+}
+
+li {
+  // display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="home">
+    <button v-on:click="handleGreet">Greet</button>
+    <!-- {{state}} -->
+    <div v-if="state.seen">
+      <ul>
+        <li v-for="(todo, key) in state.todos" v-bind:key="key">{{ todo.text }}</li>
+      </ul>
+      <form class="form">
+        <input v-model="message" placeholder="Please type here...">
+        <button v-on:click="onSubmit">Submit</button>
+        <div class="check-select">
+          <input type="checkbox" id="checkbox" v-model="checked">
+          <label for="checkbox">{{ checked }}</label>
+        </div>
+      </form>
+    </div>
+    <div v-else-if="state.seen === false">
+      <p>
+        <strong>Toggle Again to see List Items</strong>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: ["state", "handleGreet"],
+  methods: {
+    onSubmit: function(event) {
+      alert(this.message)
+    }
+  },
+  data() {
+    return {
+      demo: "Hello Child Component",
+      message: "",
+      checked: false
+    };
+  }
+};
+</script>
